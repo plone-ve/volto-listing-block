fe23f59ae216d6cc903049185a4a4986b1db773f
David Ichim <ichim.david@gmail.com>
2022-09-28 11:39:58 +0300

    fix(listing): removed double rounded option from search item

--- src/blocks/Listing/SearchItemTemplate.jsx
+++ src/blocks/Listing/SearchItemTemplate.jsx
@@ -61,15 +61,5 @@ export const SearchItemLayout = (props) => {
   return <BasicItem {...props} />;
 };
 SearchItemLayout.styleSchemaEnhancer = ({ schema, intl }) => {
-  const styleSchema = schema.properties.styles.schema;
-  styleSchema.fieldsets[0].fields.push('rounded');
-  styleSchema.properties = {
-    ...styleSchema.properties,
-    rounded: {
-      title: intl.formatMessage(messages.Rounded),
-      description: intl.formatMessage(messages.RoundedHelp),
-      type: 'boolean',
-    },
-  };
   return schema;
 };
