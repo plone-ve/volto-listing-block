ea41d9c4013cbdb6adc96e41303abef470a1db70
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2022-11-02 17:39:14 +0200

    Try to cleanup styling

--- src/components/UniversalCard/UniversalCard.jsx
+++ src/components/UniversalCard/UniversalCard.jsx
@@ -1,31 +1,20 @@
-import cx from 'classnames';
-
 import config from '@plone/volto/registry';
 import { resolveExtension } from '@plone/volto/helpers/Extensions/withBlockExtensions';
 import { Item } from './model';
 
-import { getVoltoStyles } from '@eeacms/volto-listing-block/schema-utils';
+// import { getVoltoStyles } from '@eeacms/volto-listing-block/schema-utils';
 import schemaEnhancer from './schema';
 
 function UniversalCard(props) {
-  const { itemModel = {}, styles, item, ...rest } = props;
+  const { itemModel = {}, item, ...rest } = props;
   const extension = resolveExtension(
     '@type',
     config.blocks.blocksConfig.listing.extensions.cardTemplates,
     itemModel,
   );
   const CardTemplate = extension.template;
-  const classNames = getVoltoStyles(styles);
 
-  return (
-    <CardTemplate
-      className={cx(classNames)}
-      item={new Item(item)}
-      itemModel={itemModel}
-      styles={styles}
-      {...rest}
-    />
-  );
+  return <CardTemplate item={new Item(item)} itemModel={itemModel} {...rest} />;
 }
 
 UniversalCard.schemaEnhancer = schemaEnhancer;
